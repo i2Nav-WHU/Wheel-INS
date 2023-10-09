324fcc1079e839fba93f7fc70bd24563bab7b6b2
Yibin Wu <1040154272@qq.com>
2023-10-09 15:37:46 +0200

    add output dir.

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Wheel-INS: A Wheel-mounted MEMS IMU-based Dead Reckoning System
+<img src="https://img.shields.io/badge/Linux-FCC624?logo=linux&logoColor=black" />
+<img src="https://img.shields.io/badge/Windows-0078D6?st&logo=windows&logoColor=white" />
 
 Unlike the conventional odometer-aided inertial navigation system (ODO/INS) where the IMU is always placed either on the vehicle body or in the coach, in Wheel-INS, the IMU is mounted on the wheel of the ground vehicle. There are two major advanatges by doing so: 1) the wheel velocity can be obtained by the Wheel-IMU thus replace the traditional odometer (or wheel encoder); 2) the rotation modulation can be leveraged to mitigate the error accumulation of INS. Particularly, the wheel velocity calculated by the gyroscope outputs and the wheel radius is treated as an external observation with non-holonomic constraint (NHC) to fuse with INS through an extended Kalman filter (EKF).
 
 ## :boom:News:boom:
-:tada::tada: Oct. 2023 A completely new version of the code is released!
+:tada::tada: Oct. 2023 A completely new version of the code supporting both Linux and Windows is released!
 
-:tada::tada: Nov. 2022 Our paper on one wheel-mounted IMU-based SLAM ([Wheel-SLAM](https://arxiv.org/pdf/2211.03174.pdf)) has been accepted to IEEE Robotics and Automation Letters. The [source code](https://github.com/i2Nav-WHU/Wheel-SLAM) is released. Check it out!
+:tada::tada: Nov. 2022 Wheel-INS has been extended to SLAM! Check out our paper of [Wheel-SLAM](https://arxiv.org/pdf/2211.03174.pdf) accepted to IEEE Robotics and Automation Letters. The [source code](https://github.com/i2Nav-WHU/Wheel-SLAM) is also released. 
 
 :tada::tada: Nov. 2022 Our paper on multiple IMUs-based wheeled robot localization ([Wheel-INS2](https://ieeexplore.ieee.org/document/9950438)) has been accepted to IEEE Transactions on Intelligent Transportation Systems. Check it out!
 
@@ -51,7 +53,7 @@ Once a execuble file **Wheel-INS.exe** is generated, the compilation is done. Th
 ```
 You can then run **plot.py** in utils to plot the trajectory estimated by Wheel-INS as well as the raw Wheel-IMU data.
 ## Datasets
-Two sets of example data with ground truth are also provided (see ***dtaset***). Please refer to the ***ReadMe.pdf*** for details. If *git clone* is too slow, please try to download the *.zip* file directly.
+Two sets of example data with ground truth are provided (see ***dtaset***). Please refer to the ***ReadMe.pdf*** for details. If *git clone* is too slow, please try to download the *.zip* file directly.
 
 ## Related Papers
 *Based on the study of Wheel-IMU, we have published three papers. 1) The original Wheel-INS paper where we proposed a wheel-mounted IMU-based dead reckoning system and investigated its characteristics. 2) A thorough and complete comparison on three different measurement models in Wheel-INS with both theoretical analysis and experimental illustration. 3) A multiple IMUs-based localization system for wheeled robots by obtaining different dynamic information of the vhehicle and taking advantage of the relative spatial constraints among the inertial sensors.*
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <Eigen/Dense>
+#include <fstream>
 #include <iomanip>
 #include <iostream>
-#include <fstream>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -13,140 +13,136 @@
 #endif
 
 #include "fileio/imufileloader.h"
-#include "wheel_ins/wheelins.h"
 #include "wheel_ins/utils.h"
-
+#include "wheel_ins/wheelins.h"
 
 int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "usage: Wheel-INS config.yaml" << std::endl;
+    return -1;
+  }
 
-    if (argc != 2) {
-        std::cout << "usage: Wheel-INS wheel-ins.yaml" << std::endl;
-        return -1;
+  YAML::Node config;
+  try {
+    config = YAML::LoadFile(argv[1]);
+
+  } catch (YAML::Exception &exception) {
+    std::cout << "Failed to read configuration file: " << exception.what()
+              << std::endl;
+    return -1;
+  }
+
+  Paras paras;
+  if (!loadConfig(config, paras)) {
+    std::cout << "Error occured in the configuration file!" << std::endl;
+    return -1;
+  }
+
+  std::string imupath, outputpath;
+  try {
+    imupath = config["imupath"].as<std::string>();
+    outputpath = config["outputpath"].as<std::string>();
+  } catch (YAML::Exception &exception) {
+    std::cout << "Failed when loading configuration: " << exception.what()
+              << std::endl;
+    return -1;
+  }
+
+#ifdef _WIN32
+  if (GetFileAttributesA(outputpath.c_str()) == INVALID_FILE_ATTRIBUTES)
+#else
+  if (access(outputpath.c_str(), F_OK))
+#endif
+  {
+    MKDIR(std::string("output"));
+    MKDIR(outputpath);
+  }
+
+  int imudatalen, imudatarate;
+  double starttime, endtime;
+  try {
+    imudatalen = config["imudatalen"].as<int>();
+    imudatarate = config["imudatarate"].as<int>();
+    starttime = config["starttime"].as<double>();
+    endtime = config["endtime"].as<double>();
+  } catch (YAML::Exception &exception) {
+    std::cout << "Failed when loading configuration. Please check the data "
+                 "length, data rate, and the process time!"
+              << std::endl;
+    return -1;
+  }
+
+  ImuFileLoader imufile(imupath, imudatalen, imudatarate);
+
+  WheelINS wheelins(paras);
+
+  const int nav_columns = 11, imuerr_columns = 13, std_columns = 22;
+  FileSaver navfile(outputpath + "/traj.txt", nav_columns, FileSaver::TEXT);
+  FileSaver imuerrfile(outputpath + "/imuerror.txt", imuerr_columns,
+                       FileSaver::TEXT);
+  FileSaver stdfile(outputpath + "/std.txt", std_columns, FileSaver::TEXT);
+
+  if (!imufile.isOpen() || !navfile.isOpen() || !imuerrfile.isOpen() ||
+      !stdfile.isOpen()) {
+    std::cout << "Failed to open data file!" << std::endl;
+    return -1;
+  }
+
+  if (endtime < 0) {
+    endtime = imufile.endtime();
+  }
+
+  if (starttime < imufile.starttime() || starttime > endtime) {
+    std::cout << "Process time ERROR!" << std::endl;
+    return -1;
+  }
+
+  IMU imu_cur;
+  do {
+    imu_cur = imufile.next();
+  } while (imu_cur.timestamp < starttime);
+
+  wheelins.addImuData(imu_cur);
+
+  double timestamp;
+  NavState navstate;
+  Eigen::MatrixXd cov;
+
+  int percent = 0, lastpercent = 0;
+  double interval = endtime - starttime;
+
+  while (true) {
+    imu_cur = imufile.next();
+    if (imu_cur.timestamp > endtime || imufile.isEof()) {
+      break;
     }
-    std::cout<<argv[1]<<std::endl;
-    YAML::Node config;
-    try {
-        config = YAML::LoadFile(argv[1]);
-        
-    } catch (YAML::Exception &exception) {
-        std::cout << "Failed to read configuration file: " <<exception.what()<< std::endl;
-        return -1;
-    }
-
-    Paras paras;
-    if (!loadConfig(config, paras)) {
-        std::cout << "Error occurs in the configuration file!" << std::endl;
-        return -1;
-    }
-
-    std::string imupath, outputpath;
-    try {
-        imupath    = config["imupath"].as<std::string>();
-        outputpath = config["outputpath"].as<std::string>();
-    } catch (YAML::Exception &exception) {
-        std::cout << "Failed when loading configuration: " <<exception.what() << std::endl;
-        return -1;
-    }
-
-    #ifdef _WIN32
-    if (GetFileAttributesA(outputpath.c_str()) == INVALID_FILE_ATTRIBUTES) {
-    #else
-    if (access(outputpath.c_str(), F_OK)) {
-    #endif
-        // Directory does not exist, create it
-        if (MKDIR(outputpath) == 0) {
-            std::cout << "Directory created successfully." << std::endl;
-        } else {
-            std::cerr << "Error creating directory." << std::endl;
-        }
-    } else {
-        std::cout << "Directory already exists." << std::endl;
-    }
-
-
-    int imudatalen, imudatarate;
-    double starttime, endtime;
-    try {
-        imudatalen  = config["imudatalen"].as<int>();
-        imudatarate = config["imudatarate"].as<int>();
-        starttime   = config["starttime"].as<double>();
-        endtime     = config["endtime"].as<double>();
-    } catch (YAML::Exception &exception) {
-        std::cout << "Failed when loading configuration. Please check the data length, data rate, and the process time!"
-                  << std::endl;
-        return -1;
-    }
-    
-    ImuFileLoader imufile(imupath, imudatalen, imudatarate);
-
-    WheelINS wheelins(paras);
-
-    const int nav_columns = 11, imuerr_columns = 13, std_columns = 22;
-    FileSaver navfile(outputpath + "/wheelins_Navresult.nav", nav_columns, FileSaver::TEXT);
-    FileSaver imuerrfile(outputpath + "/wheelins_IMU_ERR.txt", imuerr_columns, FileSaver::TEXT);
-    FileSaver stdfile(outputpath + "/wheelins_STD.txt", std_columns, FileSaver::TEXT);
-
-    if (!imufile.isOpen() || !navfile.isOpen() || !imuerrfile.isOpen() || !stdfile.isOpen()) {
-        std::cout << "Failed to open data file!" << std::endl;
-        return -1;
-    }
-
-    if (endtime < 0) {
-        endtime = imufile.endtime();
-    }
-
-    if (starttime < imufile.starttime() || starttime > endtime) {
-        std::cout << "Process time ERROR!" << std::endl;
-        return -1;
-    }
-
-    IMU imu_cur;
-    do {
-        imu_cur = imufile.next();
-    } while (imu_cur.timestamp < starttime);
-
-
     wheelins.addImuData(imu_cur);
 
-    double timestamp;
-    NavState navstate;
-    Eigen::MatrixXd cov;
+    wheelins.newImuProcess();
 
-    int percent = 0, lastpercent = 0;
-    double interval = endtime - starttime;
+    timestamp = wheelins.timestamp();
+    navstate = wheelins.getNavState();
+    cov = wheelins.getCovariance();
 
-    while (true) {
+    writeNavResult(timestamp, navstate, navfile, imuerrfile);
+    writeSTD(timestamp, cov, stdfile);
 
-        imu_cur = imufile.next();
-        if (imu_cur.timestamp > endtime || imufile.isEof()) {
-            break;
-        }
-        wheelins.addImuData(imu_cur);
-
-        wheelins.newImuProcess();
-
-        timestamp = wheelins.timestamp();
-        navstate  = wheelins.getNavState();
-        cov       = wheelins.getCovariance();
-
-        writeNavResult(timestamp, navstate, navfile, imuerrfile);
-        writeSTD(timestamp, cov, stdfile);
-
-        percent = int((imu_cur.timestamp - starttime) / interval * 100);
-        if (percent - lastpercent >= 1) {
-            std::cout << "Processing: " << std::setw(3) << percent << "%\r" << std::flush;
-            lastpercent = percent;
-        }
+    percent = int((imu_cur.timestamp - starttime) / interval * 100);
+    if (percent - lastpercent >= 1) {
+      std::cout << "Processing: " << std::setw(3) << percent << "%\r"
+                << std::flush;
+      lastpercent = percent;
     }
+  }
 
-    imufile.close();
-    navfile.close();
-    imuerrfile.close();
-    stdfile.close();
+  imufile.close();
+  navfile.close();
+  imuerrfile.close();
+  stdfile.close();
 
+  std::cout << std::endl << std::endl << "Wheel-INS Process Finish! ";
+  std::cout << "From " << starttime << " s to " << endtime << " s, total "
+            << interval << " s!" << std::endl;
 
-    std::cout << std::endl << std::endl << "Wheel-INS Process Finish! ";
-    std::cout << "From " << starttime << " s to " << endtime << " s, total " << interval << " s!" << std::endl;
-    
-    return 0;
+  return 0;
 }
